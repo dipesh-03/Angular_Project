4f8b3cc81f7e52ad40e590d8eec6d5e8aa5f80b0
Dipesh <dipeshkumar.ce@gmail.com>
2023-03-21 15:05:18 +0530

    components and databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , Output , EventEmitter} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  @Output() RecipeWasSelected = new EventEmitter<Recipe>();
+
   recipes : Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test','https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800')
+    new Recipe('A Test Recipe','This is simply a test','https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800'),
+    new Recipe('Another Test Recipe','This is simply a test','https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800')
   ];
 
   constructor(){};
+
+  onRecipeSelected(recipe:Recipe){
+    this.RecipeWasSelected.emit(recipe);
+  }
 
 }
