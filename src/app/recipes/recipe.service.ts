756a1f169a626381c488b984b56f17a06775cec5
Dipesh <dipeshkumar.ce@gmail.com>
2023-03-22 14:09:51 +0530

    add ingredient to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService{
@@ -6,8 +7,14 @@ export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes : Recipe[] = [
-        new Recipe('A Test Recipe','This is simply a test','https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800'),
-        new Recipe('Another Test Recipe','This is simply a test','https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800')
+        new Recipe('Aloo patties','This is simply a test','https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800',[
+            new Ingredient('Potato',2),
+            new Ingredient('Corn Flour',1)
+        ]),
+        new Recipe('Dabeli','This is simply a test','https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800',[
+            new Ingredient('Pav',1),
+            new Ingredient('Potato',1)
+        ])
       ];
 
     getrecipes(){
