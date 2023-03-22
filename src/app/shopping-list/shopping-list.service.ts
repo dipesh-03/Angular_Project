e7b8887f3e2e5f1cfc3888d2b83e6deb12c60057
Dipesh <dipeshkumar.ce@gmail.com>
2023-03-22 14:24:06 +0530

    adding ingredients from recipes to shopping list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -16,4 +16,9 @@ export class ShoppingListService{
         this.ingredients.push(ingredient);
         this.ingredientsChanged.emit(this.ingredients.slice());
       }
+
+      addIngredients(ingredient: Ingredient[]){
+        this.ingredients.push(...ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+      }
 }
