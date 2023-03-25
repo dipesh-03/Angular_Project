42e44224899ca19097c6e9d4c1d6414797eed88e
Dipesh <dipeshkumar.ce@gmail.com>
2023-03-25 10:51:10 +0530

    shopping list form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,10 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import {  Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService{
-
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes : Recipe[] = [
         new Recipe('Aloo patties','This is simply a test','https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800',[
