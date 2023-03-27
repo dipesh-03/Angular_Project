9f69fe2f969b61d9de644a6309e2c30bc96c5968
Dipesh <dipeshkumar.ce@gmail.com>
2023-03-27 10:39:37 +0530

    delete and clear

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ɵNoopNgZone } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Recipe } from '../recipe.model';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -14,7 +13,7 @@ export class RecipeEditComponent implements OnInit {
   editmode = false;
   recipeForm : FormGroup
 
-  constructor(private route:ActivatedRoute , private recipeService:RecipeService){};
+  constructor(private route:ActivatedRoute , private recipeService:RecipeService , private router:Router){};
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -37,7 +36,7 @@ export class RecipeEditComponent implements OnInit {
     }else{
       this.recipeService.addRecipe(this.recipeForm.value);
     }
-
+    this.onCancel();
   }
 
   onAddIngredient(){
@@ -47,6 +46,10 @@ export class RecipeEditComponent implements OnInit {
         'amount': new FormControl(null,[Validators.required,Validators.pattern('^([1-9][0-9]*)$')])
       })
     )
+  }
+
+  onCancel(){
+    this.router.navigate(['../'],{relativeTo:this.route})
   }
 
   private inItForm(){
