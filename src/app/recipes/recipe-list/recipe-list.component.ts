33fa3d9d9136ce2e16795873fc2ba23524e964b1
Dipesh <dipeshkumar.ce@gmail.com>
2023-03-27 11:04:37 +0530

    small changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component , OnInit} from '@angular/core';
+import { Component , OnDestroy, OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,15 +9,15 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
-
+export class RecipeListComponent implements OnInit , OnDestroy{
+  subscription : Subscription;
   recipes : Recipe[] = [];
 
   constructor( private recipeService : RecipeService , private router:Router , private route:ActivatedRoute){};
 
   ngOnInit() {
 
-    this.recipeService.recipesChanged.subscribe(
+    this.subscription = this.recipeService.recipesChanged.subscribe(
       (recipe:Recipe[]) => {
         this.recipes = recipe;
       }
@@ -28,4 +29,7 @@ export class RecipeListComponent implements OnInit {
     this.router.navigate(['new'] , {relativeTo:this.route});
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
 }
