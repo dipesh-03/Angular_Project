1b7ca5e71cb2b5a223e4d7565f03eb9bfd48daca
Dipesh <dipeshkumar.ce@gmail.com>
2023-03-27 17:11:06 +0530

    storing and fetching recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from "@angular/core";
 import {HttpClient} from '@angular/common/http'
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
 
 @Injectable({providedIn:'root'})
 export class DataStorageService{
-    constructor(private http:HttpClient){}
+    constructor(private http:HttpClient , private recipeService:RecipeService){}
+
+    storeRecipes(){
+        const recipes = this.recipeService.getrecipes();
+        return this.http.put('https://nothing-be935-default-rtdb.firebaseio.com/recipes.json',recipes).subscribe();
+    }
+
+    fetchRecipes(){
+        this.http.get<Recipe[]>('https://nothing-be935-default-rtdb.firebaseio.com/recipes.json').subscribe(
+            recipes => {
+                this.recipeService.setRecipes(recipes);
+            }
+        );
+    }
 }
