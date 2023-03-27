6f31b7e69c05f7a5e1876872b22b11567eaf180d
Dipesh <dipeshkumar.ce@gmail.com>
2023-03-27 17:17:58 +0530

    transforming data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,16 +9,17 @@ export class RecipeService{
 
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes : Recipe[] = [
-        new Recipe('Aloo patties','This is simply a test','https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800',[
-            new Ingredient('Potato',2),
-            new Ingredient('Corn Flour',1)
-        ]),
-        new Recipe('Dabeli','This is simply a test','https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800',[
-            new Ingredient('Pav',1),
-            new Ingredient('Potato',1)
-        ])
-      ];
+    // private recipes : Recipe[] = [
+    //     new Recipe('Aloo patties','This is simply a test','https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800',[
+    //         new Ingredient('Potato',2),
+    //         new Ingredient('Corn Flour',1)
+    //     ]),
+    //     new Recipe('Dabeli','This is simply a test','https://static.toiimg.com/thumb/84784534.cms?imgsize=468021&width=800&height=800',[
+    //         new Ingredient('Pav',1),
+    //         new Ingredient('Potato',1)
+    //     ])
+    //   ];
+    recipes : Recipe[] = [];
 
     constructor(private slService: ShoppingListService){};
 
