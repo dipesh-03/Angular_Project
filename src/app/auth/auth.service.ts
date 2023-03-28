ea01e7f100acb80ff9dc7afdce3dd5904bcd9bd9
Dipesh <dipeshkumar.ce@gmail.com>
2023-03-28 12:16:14 +0530

    logIn error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
@@ -19,21 +19,31 @@ export class AuthService{
   signup(email:string , password:string){
     return this.http.post<AuthResponsedata>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDkIHYXNHz7_QmkOqmAagzk1k4svFe6brA',
     {email:email , password:password , returnSecureToken:true})
-    .pipe(catchError(errorRes =>{
-      let errormessage = ' An Error Occurred'
+    .pipe(catchError(this.handleError));
+  };
+
+  login(email : string, password:string){
+    return this.http.post<AuthResponsedata>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDkIHYXNHz7_QmkOqmAagzk1k4svFe6brA',
+    {email:email , password:password , returnSecureToken:true})
+    .pipe(catchError(this.handleError));
+  }
+
+  private handleError( errorRes : HttpErrorResponse){
+    let errormessage = ' An Unknown Error Occurred'
       if(!errorRes.error || !errorRes.error.error){
         return throwError(errormessage);
       }
       switch(errorRes.error.error.message){
         case 'EMAIL_EXISTS':
           errormessage='This Email Already Exists';
+          break;
+        case 'EMAIL_NOT_FOUND':
+          errormessage='This Email Does Not Exist';
+          break;
+        case 'INVALID_PASSWORD':
+          errormessage='This Password Is Not Correct'
+          break;
       }
       return throwError(errormessage);
-    }));
-  };
-
-  login(email : string, password:string){
-    return this.http.post<AuthResponsedata>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDkIHYXNHz7_QmkOqmAagzk1k4svFe6brA',
-    {email:email , password:password , returnSecureToken:true})
   }
 }
