5d46f84d9b4352142ff74fbaac405db8de9e9bff
Dipesh <dipeshkumar.ce@gmail.com>
2023-03-28 11:39:18 +0530

    loading spinner and basic error

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -10,6 +10,8 @@ import { AuthService } from "./auth.service";
 export class AuthComponent{
 
   isLoginMode = true;
+  isLoading ;
+  error : string = null;
 
   constructor(private authService : AuthService){}
 
@@ -24,15 +26,18 @@ export class AuthComponent{
 
     const email = form.value.email;
     const password = form.value.password;
-
+    this.isLoading = true;
     if(this.isLoginMode){
 
     }else{
       this.authService.signup(email,password).subscribe(
       resData => {
           console.log(resData);
+          this.isLoading = false;
         },error=>{
           console.log(error);
+          this.error='An Error Occured';
+          this.isLoading = false;
         }
       );
     }
