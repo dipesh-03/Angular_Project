1c0197c57c8423ffe30a65b8a084cd9b3e6f9422
Dipesh <dipeshkumar.ce@gmail.com>
2023-03-28 11:50:53 +0530

    improve error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 interface AuthResponsedata{
   idToken : string;
@@ -15,6 +17,17 @@ export class AuthService{
 
   signup(email:string , password:string){
     return this.http.post<AuthResponsedata>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDkIHYXNHz7_QmkOqmAagzk1k4svFe6brA',
-    {email:email , password:password , returnSecureToken:true});
+    {email:email , password:password , returnSecureToken:true})
+    .pipe(catchError(errorRes =>{
+      let errormessage = ' An Error Occurred'
+      if(!errorRes.error || !errorRes.error.error){
+        return throwError(errormessage);
+      }
+      switch(errorRes.error.error.message){
+        case 'EMAIL_EXISTS':
+          errormessage='This Email Already Exists';
+      }
+      return throwError(errormessage);
+    }));
   };
 }
